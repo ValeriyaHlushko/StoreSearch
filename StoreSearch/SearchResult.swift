be6e3c734b524c5f282ef3239da15fefa0921717
Valeriya Hlushko <hlushko.valery@gmail.com>
2020-05-10 17:23:23 +0300

    Add a search bar and table view. The search puts fake results in the table for now

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Валерия Глушко on 10.05.2020.
+//  Copyright © 2020 Hlushko Valery. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
