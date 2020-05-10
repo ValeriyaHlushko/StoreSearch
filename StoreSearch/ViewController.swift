187030f158184cd6bfbaa3c3da7963da57c137d6
Valeriya Hlushko <hlushko.valery@gmail.com>
2020-05-10 14:50:33 +0300

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Валерия Глушко on 10.05.2020.
+//  Copyright © 2020 Hlushko Valery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
